6f0732dc4796857deef163be3db6b6802bb9bf55
Marko Troskot <mtroskot@rccl.com>
2020-01-27 10:23:37 +0100

    Updated navigation

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Image, Platform } from 'react-native';
 import { IconButton } from 'src/components';
-import HomeScreen from 'src/screens/Home';
 import CommentsScreen from 'src/screens/Comments';
-import SightingsScreen from 'src/screens/Sightings';
-import MapScreen from 'src/screens/Map';
 import FlowerDetailsScreen from 'src/screens/FlowerDetails';
+import HomeScreen from 'src/screens/Home';
+import MapScreen from 'src/screens/Map';
+
+import MenuScreen from 'src/screens/Menu';
+import FlowersScreen from 'src/screens/Flowers';
+import LatestSightingsScreen from 'src/screens/LatestSightings';
+import FavoritesScreen from 'src/screens/Favorites';
+import SettingsScreen from 'src/screens/Settings';
+import LoginScreen from 'src/screens/Login';
+import RegisterScreen from 'src/screens/Register';
+
+import SightingsScreen from 'src/screens/Sightings';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
@@ -15,19 +24,19 @@ import menuDots from 'src/assets/icons/menu/menu-dots.png';
 import { TabUtils } from 'src/utils';
 import styles from 'src/navigation/styles';
 import iconButtonStyles from 'src/components/IconButton/styles';
+import { NavigationService } from 'src/services';
 
 const HomeStack = createStackNavigator(
   {
     [screenNames.HOME]: {
       screen: HomeScreen,
       navigationOptions: () => ({
-        headerTitle: <Image source={titleLogo} style={styles.headerTitle} />,
         ...Platform.select({
           ios: {
             headerRight: (
               <IconButton
                 icon={menuDots}
-                onPress={() => alert('Coming soon')}
+                onPress={() => NavigationService.navigate(screenNames.MENU)}
                 viewStyle={iconButtonStyles.rightButton}
                 imageStyle={iconButtonStyles.menu}
               />
@@ -37,7 +46,7 @@ const HomeStack = createStackNavigator(
             headerLeft: (
               <IconButton
                 icon={menuDots}
-                onPress={() => alert('Coming soon')}
+                onPress={() => NavigationService.navigate(screenNames.MENU)}
                 viewStyle={iconButtonStyles.leftButton}
                 imageStyle={iconButtonStyles.menu}
               />
@@ -46,16 +55,46 @@ const HomeStack = createStackNavigator(
         })
       })
     },
-    [screenNames.FLOWER_DETAILS]: {
-      screen: FlowerDetailsScreen,
+    [screenNames.FLOWER_DETAILS]: FlowerDetailsScreen,
+    [screenNames.MENU]: {
+      screen: MenuScreen,
       navigationOptions: () => ({
-        headerTitle: <Image source={titleLogo} />
+        ...Platform.select({
+          ios: {
+            headerRight: (
+              <IconButton
+                icon={menuDots}
+                onPress={NavigationService.goBack}
+                viewStyle={iconButtonStyles.rightButton}
+                imageStyle={iconButtonStyles.menu}
+              />
+            ),
+            headerLeft: null
+          },
+          android: {
+            headerLeft: (
+              <IconButton
+                icon={menuDots}
+                onPress={NavigationService.goBack}
+                viewStyle={iconButtonStyles.leftButton}
+                imageStyle={iconButtonStyles.menu}
+              />
+            )
+          }
+        })
       })
-    }
+    },
+    [screenNames.FLOWERS]: FlowersScreen,
+    [screenNames.LATEST_SIGHTINGS]: LatestSightingsScreen,
+    [screenNames.FAVORITES]: FavoritesScreen,
+    [screenNames.SETTINGS]: SettingsScreen,
+    [screenNames.LOGIN]: LoginScreen,
+    [screenNames.REGISTER]: RegisterScreen
   },
   {
     headerLayoutPreset: 'center',
     defaultNavigationOptions: {
+      headerTitle: <Image source={titleLogo} style={styles.headerTitle} />,
       headerStyle: styles.headerStyle
     }
   }
@@ -77,7 +116,11 @@ const AppStack = createBottomTabNavigator(
         tabBarIcon: ({ focused }) => {
           const { icon } = TabUtils.getTabProps(routeName, focused);
           return (
-            <IconButton onPress={() => navigation.navigate(routeName)} icon={icon} imageStyle={iconButtonStyles.tabs} />
+            <IconButton
+              onPress={() => NavigationService.navigate(routeName)}
+              icon={icon}
+              imageStyle={iconButtonStyles.tabs}
+            />
           );
         },
         tabBarOptions: {
