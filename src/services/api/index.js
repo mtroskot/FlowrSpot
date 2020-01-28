8dd73d2ff189b3649f73e69baa9b5537aca9827a
Marko Troskot <mtroskot@rccl.com>
2020-01-28 13:59:08 +0100

    Covered android no internet api call

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+const CancelToken = axios.CancelToken;
 
 const defaultOptions = {
   baseURL: 'https://flowrspot-api.herokuapp.com/api/v1/',
@@ -13,13 +14,19 @@ const defaultOptions = {
 
 /**
  * Calls api and checks if response status is OK.
+ * setTimeout needed for android to cancel request, if you don't have internet connection or the IP address or
+ * domain name that you're requesting not there,in this case axios timeout will not work.
  * @param {string} url The api endpoint.
  * @param {object} options The request options.
  * @param {string} failMessage The message that will be thrown is response status is not OK.
  * @throws error if response status is not OK.
  */
 async function callApiAndCheckResponse({ url, options }) {
-  const fetchOptions = { ...defaultOptions, ...options };
+  let source = CancelToken.source();
+  setTimeout(() => {
+    source.cancel();
+  }, 6000);
+  const fetchOptions = { ...defaultOptions, cancelToken: source.token, ...options };
   const response = await axios(url, fetchOptions);
   return response.data;
 }
